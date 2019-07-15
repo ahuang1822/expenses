d432e3c733ea88b3471c7ec820246ab93483f651
Huang, Anthony <anthony.huang@accenture.com>
2019-07-15 14:36:24 -0500

    clean-up

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -2,40 +2,42 @@ import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import '../Confirmation.css';
-import { resetInputFields } from '../store/expense'
+import { resetInputFields } from '../store/expense';
 
 class Confirmation extends Component {
-  
-  goHome = (event) => {
+  goHome = event => {
     event.preventDefault();
     browserHistory.push('/');
-  }
+  };
 
-  goBack = (event) => {
+  goBack = event => {
     event.preventDefault();
-    browserHistory.push('/description')
-  }
+    browserHistory.push('/description');
+  };
 
-  onSubmit = (event) => {
-    event.preventDefault();    
+  onSubmit = event => {
+    event.preventDefault();
     const description = this.props.description;
     const amount = '$' + this.props.amount;
-    const spreadsheetId = this.props.spreadsheetId;    
+    const spreadsheetId = this.props.spreadsheetId;
     const body = {
-      values: [[description, amount]]
+      values: [[description, amount]],
     };
-    window.gapi.client.sheets.spreadsheets.values.append({
-      spreadsheetId: spreadsheetId,
-      range: 'Sheet1',
-      valueInputOption: 'USER_ENTERED',
-      resource: body
-   }).then(() => {      
-      this.props.clearInputFields();  
-      browserHistory.push('/');          
-   }).catch((error) => {
-     console.log(error);
-   })
-  }
+    window.gapi.client.sheets.spreadsheets.values
+      .append({
+        spreadsheetId: spreadsheetId,
+        range: 'Sheet1',
+        valueInputOption: 'USER_ENTERED',
+        resource: body,
+      })
+      .then(() => {
+        this.props.clearInputFields();
+        browserHistory.push('/');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   render() {
     return (
@@ -44,35 +46,40 @@ class Confirmation extends Component {
           <form onSubmit={this.onSubmit}>
             <label>
               <h5>
-              Did you really just spend ${this.props.amount} on {this.props.description}?
+                Please confirm your expense of ${this.props.amount} on{' '}
+                {this.props.description}?
               </h5>
             </label>
             <input className="btn btn-success" type="submit" value="Yes" />
           </form>
         </div>
         <div className="home-button">
-          <button className="btn btn-danger" onClick={this.goBack}>Back</button>
-        </div>        
+          <button className="btn btn-danger" onClick={this.goBack}>
+            Back
+          </button>
+        </div>
       </div>
     );
-  };
-};
+  }
+}
 
-const mapState = (state) => {
+const mapState = state => {
   return {
     description: state.expense.description,
     amount: state.expense.amount,
     spreadsheetId: state.expense.spreadsheetId,
-  }
-};
-
-const mapDispatch = (dispatch) => {
-  return {
-    clearInputFields() {     
-      dispatch(resetInputFields());      
-    }
   };
 };
 
-export default connect(mapState, mapDispatch)(Confirmation);
+const mapDispatch = dispatch => {
+  return {
+    clearInputFields() {
+      dispatch(resetInputFields());
+    },
+  };
+};
 
+export default connect(
+  mapState,
+  mapDispatch,
+)(Confirmation);
